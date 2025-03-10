46851ecaf58634d987102dfd528240e13764c40d
Bisu03 <berabiswanath338@gmail.com>
2025-03-10 14:45:59 +0530

    fix the api getway

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,26 +1,26 @@
-import apiRequest from "./apiRequest";
+import axios from "axios";
 
 // Fetch data
 export const fetchData = async (endpoint) => {
-  const response = await apiRequest.get(`${endpoint}`);
+  const response = await axios.get(`/api/v1/${endpoint}`);
   return response.data;
 };
 
 
 // Create data
 export const createData = async (endpoint, newData) => {
-  const response = await apiRequest.post(`${endpoint}`, newData);
+  const response = await axios.post(`/api/v1/${endpoint}`, newData);
   return response.data;
 };
 
 // Update data
 export const updateData = async (endpoint, id, updatedData) => {
-  const response = await apiRequest.put(`${endpoint}/${id}`, updatedData);
+  const response = await axios.put(`/api/v1/${endpoint}/${id}`, updatedData);
   return response.data;
 };
 
 // Delete data
 export const deleteData = async (endpoint, id) => {
-  const response = await apiRequest.delete(`${endpoint}/${id}`);
+  const response = await axios.delete(`/api/v1/${endpoint}/${id}`);
   return response.data;
 };
